bcfd8ba1a4d18a4acd53500ea2ab0902bf421893
miyako <kotohogi5@gmail.com>
2025-04-06 00:14:35 +0900

    【講義23】Lombokのインストールと新studentsテーブルでの全件表示機能を実装

--- src/main/java/raisetech/student/manegement/Application.java
+++ src/main/java/raisetech/student/manegement/Application.java
@@ -22,33 +22,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-
-	//【第20回課題】すべての生徒情報を表示する
-	@GetMapping("/student")
-	public List<Student> searchAllStudentInfo(){
+	@GetMapping("/students")
+	public List<Student> searchStudentInfo(){
 		return repository.searchAllStudentInfo();
-}
-
-
-	//生徒情報の登録する
-	@PostMapping("/student")
-	public void registerStudentInfo(String name,int age){
-		repository.registerStudentInfo(name,age);
 	}
-
-	
-	//生徒の年齢を修正する
-	@PatchMapping("/student")
-	public void updateStudentAge(String name , int age){
-		repository.updateStudentAge(name,age);
-	}
-
-
-	//生徒情報を削除する
-	@DeleteMapping("/student")
-	public void deleteStudentInfo(String name){
-		repository.deleteStudentInfo(name);
-	}
-
 
 }
--- src/main/java/raisetech/student/manegement/StudentRepository.java
+++ src/main/java/raisetech/student/manegement/StudentRepository.java
@@ -12,22 +12,6 @@ import org.apache.ibatis.annotations.Update;
 //データベースを操作するクラス
 public interface StudentRepository {
 
-  /*指定した生徒情報を１件取得
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
-  */
-
-  //【第20回課題】の解答
-  @Select("SELECT name, age FROM student")
+  @Select("SELECT * FROM students")
   List<Student> searchAllStudentInfo();
-
-  @Insert("INSERT student values(#{name},#{age})")
-  void registerStudentInfo(String name,int age);
-
-  @Update("UPDATE student SET age=#{age} WHERE name = #{name}")
-  void updateStudentAge(String name , int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudentInfo(String name);
-
 }
