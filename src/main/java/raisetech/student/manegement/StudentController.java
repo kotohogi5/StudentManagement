1962759ace5bb144df4fde79f2e8a1433c5d6ac6
miyako <kotohogi5@gmail.com>
2025-04-12 17:35:58 +0900

    【課題24】サービス層で30代生徒情報のみをフィルタリングしてcurlで出力する

--- src/main/java/raisetech/student/manegement/StudentController.java
+++ src/main/java/raisetech/student/manegement/StudentController.java
@@ -1,6 +1,7 @@
 package raisetech.student.manegement;
 
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +17,22 @@ public class StudentController {
     this.service = service;
   }
 
+  //全生徒情報を出力するようリクエスト/レスポンスする
   @GetMapping("/studentList")
   public List<Student> searchStudentList(){
     return service.searchStudentList();
   }
 
+  //全コース情報を出力するようリクエスト/レスポンスする
   @GetMapping("/courseList")
   public List<Course> searchCourseList(){
     return service.searchCourseList();
   }
 
+  //【課題24】30代の生徒情報をフィルタリングしてから出力するようリクエスト/レスポンスする
+  @GetMapping("/studentAge30To39")
+  public Map<String,Student> searchStudentAge30To39(){
+    return service.searchStudentAge30To39();
+  }
 
 }
--- src/main/java/raisetech/student/manegement/StudentService.java
+++ src/main/java/raisetech/student/manegement/StudentService.java
@@ -1,6 +1,9 @@
 package raisetech.student.manegement;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.student.manegement.repository.StudentRepository;
@@ -15,11 +18,25 @@ public class StudentService {
     this.repository = repository;
   }
 
+  //全生徒情報を出力する
   public List<Student> searchStudentList(){
     return repository.seachStudentList();
   }
 
+  //全コース情報を出力する
   public List<Course> searchCourseList(){
     return repository.seachCourseList();
+  }
+
+  //【課題24】30代の生徒情報だけをフィルタリングしてコントローラーへ返す
+  public Map<String,Student> searchStudentAge30To39(){
+
+    //全生徒情報をリポジトリ層（DB）から取得する
+    List<Student> studentList = repository.seachStudentList();
+
+    //フィルタリングした30代の生徒情報を返す
+    return studentList.stream()
+        .filter(student -> student.getAge() >= 30 && student.getAge() < 40)
+        .collect(Collectors.toMap(Student::getId,Function.identity()));
   }
 }
